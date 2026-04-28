7ae09daa080f4278e72589a7d670f37f5e00e735
Lucas Guilhermino <lucasguilherminooliveira@gmail.com>
2026-04-28 18:34:49 -0300

    Entidade Order

--- src/main/java/com/desafio/injecaodepedencia/entities/Order.java
+++ src/main/java/com/desafio/injecaodepedencia/entities/Order.java
@@ -2,6 +2,21 @@ package com.desafio.injecaodepedencia.entities;
 
 public class Order {
 
-   private
+   private Integer code;
+   private Double basic;
+   private Double discount;
 
+   public Order(){}
+
+   public Order(Double discount, Double basic, Integer code) {
+      this.discount = discount;
+      this.basic = basic;
+      this.code = code;
+   }
+
+   public Order(Integer code, Double basic, Double discount) {
+      this.code = code;
+      this.basic = basic;
+      this.discount = discount;
+   }
 }
